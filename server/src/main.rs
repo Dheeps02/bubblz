a5666f6b5bc5502d4f9c639374753931f5c4ff89
Dheepan <dheepanraajs@gmail.com>
2026-03-05 23:02:19 +0530

    chore: remove unused imports and fix warnings

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,7 @@
 mod models;
 mod db;
 
-// use models::user::User;
-// use models::room::Room;
-// use models::message::{Message, MessageType};
-
 #[tokio::main]
 async fn main() {
-    let pool = db::establish_db_connection("sqlite:bubblz.db?mode=rwc", "schema.sql").await.expect("Failed to establish DB connection with bubblz.db");
+    let _pool = db::establish_db_connection("sqlite:bubblz.db?mode=rwc", "schema.sql").await.expect("Failed to establish DB connection with bubblz.db");
 }
