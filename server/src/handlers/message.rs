e583daaa5842e4f0bea99f1f4a80a492636c2204
Dheepan <dheepanraajs@gmail.com>
2026-03-07 00:46:17 +0530

    feat: add create message and get messages endpoints

---
+++ server/src/handlers/message.rs
@@ -0,0 +1,33 @@
+use axum::{extract::{Json, State, Path}, response::IntoResponse, http::StatusCode};
+use sqlx::SqlitePool;
+use crate::handlers::CreateMessage;
+use crate::models::message::Message;
+
+pub async fn create_message(State(pool): State<SqlitePool>, Path(room_id): Path<i64>, Json(message): Json<CreateMessage>) -> Result<impl IntoResponse, StatusCode> {
+    let mut conn = pool.acquire().await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    sqlx::query("INSERT INTO messages (message_type, sender, room, timestamp, content) VALUES (?, ?, ?, ?, ?)")
+        .bind("Text")                // Message Type texts are the only ones hitting the endpoint. Any system type would be auto generated hopefully. Correct me if am wrong
+        .bind(message.sender_id)
+        .bind(room_id)
+        .bind(0)
+        .bind(message.content)
+        .execute(&mut *conn)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(StatusCode::CREATED)
+}
+
+pub async fn get_messages(State(pool): State<SqlitePool>, Path(room_id): Path<i64>) -> Result<impl IntoResponse, StatusCode> {
+    let mut conn = pool.acquire().await.map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    let messages = sqlx::query_as::<_, Message>("SELECT * FROM messages WHERE room = ?")
+        .bind(room_id)
+        .fetch_all(&mut *conn)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(Json(messages))
+
+}
