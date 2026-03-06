111ab1ae57e71aa8e54fe8b3c2fa3911684ee032
Dheepan <dheepanraajs@gmail.com>
2026-03-07 00:45:45 +0530

    feat: add request body structs for handlers

---
+++ server/src/handlers/mod.rs
@@ -0,0 +1,25 @@
+use serde::Deserialize;
+
+pub mod user;
+pub mod room;
+pub mod message;
+
+#[derive(Deserialize)]
+pub struct CreateUser {
+    pub username: String,
+    pub password: String,
+    pub email: String,
+}
+
+#[derive(Deserialize)]
+pub struct CreateRoom {
+    pub name: String,
+    pub description: String,
+    pub owner_id: i64,
+}
+
+#[derive(Deserialize)]
+pub struct CreateMessage {
+    pub content: String,
+    pub sender_id: i64,
+}
