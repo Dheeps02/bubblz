3fd3481d648588420e5d2ce671f5d8f0e3b7e4ad
Dheepan <dheepanraajs@gmail.com>
2026-03-13 15:59:32 +0530

    feat: add SendFailed variant to BubblzError

--- server/src/errors.rs
+++ server/src/errors.rs
@@ -1,12 +1,14 @@
 #[derive(Debug)]
 pub enum BubblzError {
     LockPoisoned(String),
+    SendFailed(String),
 }
 
 impl std::fmt::Display for BubblzError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
             BubblzError::LockPoisoned(msg) => write!(f, "Lock poisoned: {}", msg),
+            BubblzError::SendFailed(msg) => write!(f, "Failed to write to sender: {}", msg),
         }
     }
 }
