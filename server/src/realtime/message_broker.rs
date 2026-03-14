8c2d8d3102447b82b666edc27a77d3263ac9dcb1
Dheepan <dheepanraajs@gmail.com>
2026-03-14 17:10:10 +0530

    feat: add tracing instrumentation to message broker

--- server/src/realtime/message_broker.rs
+++ server/src/realtime/message_broker.rs
@@ -17,6 +17,7 @@ pub struct MessageBroker {
 
 impl MessageBroker {
     pub fn new() -> Self {
+        tracing::debug!("initialized new MessageBroker object");
         MessageBroker {
             connection_registry: ConnectionRegistry::new(),
             subscription_registry: SubscriptionRegistry::new(),
@@ -24,17 +25,21 @@ impl MessageBroker {
     }
 
     pub async fn publish(&self, room_id: i64, message: Message) -> Result<(), BubblzError> {
+        tracing::debug!(room_id = room_id, "attempting to publish message to room");
         for user_id in self.subscription_registry.get_users(room_id)? {
             self.connection_registry.send_to_user(user_id, message.clone()).await?;
         }
+        tracing::debug!(room_id = room_id, "message published to room successfully");
         Ok(())
     }
 
     pub async fn add_user(&self, user_id: i64, sink: SplitSink<WebSocket, Message>) {
+        tracing::debug!(user_id = user_id, "adding user to broker");
         self.connection_registry.add(user_id, sink).await
     }
 
     pub async fn remove_user(&self, user_id: i64) {
+        tracing::debug!(user_id = user_id, "removing user from broker");
         self.connection_registry.remove(user_id).await
     }
 
